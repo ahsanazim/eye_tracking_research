d56a180ac0555ff79d57bf1e58d5ecc325bb86cb
AhsanAzim <ahsan-azim@live.com>
2019-01-23 02:20:51 -0500

    socket receiving data

--- extension/src/inject/inject.js
+++ extension/src/inject/inject.js
@@ -25,7 +25,11 @@ $.ajax({
         const titleHTML = `<h1 class="title">${result.title}</h1>`;
         const subHeadHTML = ``;
 
-        // generate content spans by TOKENIZING
+        /**************************************************************
+         * 
+         *                  REPLACE PAGE CONTENT
+         * 
+         **************************************************************/
         var contentHTML = "";
         var rawContentStr = result.content;
         var spanContent = "";
@@ -74,6 +78,118 @@ $.ajax({
 
         $("html").html(newPage);
 
+
+        /**************************************************************
+         * 
+         *                      WEBSOCKET
+         * 
+         **************************************************************/
+
+        // array of arrays with last 10 x,y positions of gaze tracker
+        var coords_q = [[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1]];
+        var data = [];    // array of [line_num, timestamp] objects
+        if ("WebSocket" in window) {
+            //alert("WebSocket is supported by your Browser!");
+            
+            // Let us open a web socket
+            var ws = new WebSocket("ws://localhost:8765/hello");
+
+            ws.onopen = function() {
+                // Web Socket is connected, send data using send()
+                ws.send("Socket Opened");
+                //alert("Message is sent...");
+            };
+
+            ws.onmessage = function (evt) { 
+                var received_msg = evt.data;
+                console.log(received_msg);          // uncomment to log all coordinates
+                var tokens = received_msg.split('|');
+                if (tokens[0] === 'during') {
+                    // get normalized gaze position
+                    var tobii_x = tokens[1];
+                    var tobii_y = tokens[2];
+                    var x = tobii_x + window.screenLeft + (window.outerWidth - window.innerWidth);
+                    var y = tobii_y + window.screenTop - (window.outerHeight - window.innerHeight);
+                    // smoothing -- compute stabilized x and y coords
+                    // use coords_q as queue sliding over past 10 x,y positions
+                    // mean of these is our current position1
+                    var x_sum = 0.0;
+                    var y_sum = 0.0;
+                    var avg_denom = 0.0; // needed b/c of initial cases where coords_q not filled fully
+                    coords_q.push([x,y]);
+                    coords_q.shift();
+                    for(var i = 0; i < coords_q.length; i++) {
+                        if (coords_q[i][0] !== -1) {
+                            x_sum += parseFloat(coords_q[i][0]);
+                            y_sum += parseFloat(coords_q[i][1]);
+                            avg_denom += 1;
+                        }
+                    }
+                    x = x_sum / avg_denom;
+                    y = y_sum / avg_denom;
+                    // add an offset to deal with slight innacuracy (one line' worth)
+                    // y += 28;
+                    // draw dot at current gaze position    DO NOT DELETE!!!! -- uncomment whenever you need tracer
+                    /*
+                    var dot_color = 'green';
+                    var dot_size = '5px';
+                    $("body").append(
+                    $('<div></div>')
+                        .css('position', 'absolute')
+                        .css('top', y + 'px')
+                        .css('left', x + 'px')
+                        .css('width', dot_size)
+                        .css('height', dot_size)
+                        .css('background-color', dot_color)
+                    );
+                    */
+                    
+                    // highlight the relevant line (unhighlighting everything else)
+                    var elems = $("span");
+                    Array.from(elems).forEach(function (el) {
+                        $(el).css("background-color", "white");
+                    });
+                    el = document.elementFromPoint(x, y)
+                    if (el.nodeName.toLowerCase() == "span") {
+                        $(el).css("background-color", "yellow");
+                    }
+                }
+            };
+
+            ws.onclose = function() { 
+                // websocket is closed.
+                // alert("Connection is closed..."); 
+            };
+
+            window.onbeforeunload = function(event) {
+                // Close the connection, if open.
+                if (ws.readyState === WebSocket.OPEN) {
+                    // TODO: conceptual thing - why was doc.getElembyid not giving
+                    // correct result unless you toggled?
+                    var infoToSend = {
+                    article: '1',
+                    user: '1',
+                    hlight: $("#hlighterCenter").css("visibility"),
+                    data: data
+                    };
+                    ws.send(JSON.stringify(infoToSend));
+                    ws.close();
+                };
+            }
+        }
+        else {
+            // The browser doesn't support WebSocket
+            alert("WebSocket NOT supported by your Browser!");
+        }
+
+
+
+        /**************************************************************
+         * 
+         *              MOUSE-BASED HIGHLIGHTING
+         * 
+         **************************************************************/
+        /*
         document.onmousemove = function(e){
             // mouse coordinates
             var x = e.pageX;
@@ -92,6 +208,7 @@ $.ajax({
                 $(el).css("background-color", "yellow");
             }
         }
+        */
     }
 },
 {
